2083537312e215f4ae14dd79b954adf64cbbfb2b
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-10-07 22:40:11 +0500

    first commit

---
+++ app/src/main/java/uz/gita/music_player_io/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.gita.music_player_io
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/uz/gita/music_player_io/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,5 @@
+package uz.gita.music_player_io.presentation.screens.splash
+
+// Created by Jamshid Isoqov an 10/7/2022
+class SplashScreen {
+}
