4ff579d1fa2ad68d82d4fe7e7ef44826bad9629d
Rasulbek <rasuldev404@gmail.com>
2022-10-19 15:36:10 +0500

    xml

---
+++ app/src/main/java/uz/gita/music_player_io/presentation/screens/playlist/detail/add/PlaylistAddScreen.kt
@@ -0,0 +1,7 @@
+package uz.gita.music_player_io.presentation.screens.playlist.detail.add
+
+import androidx.fragment.app.Fragment
+import uz.gita.music_player_io.R
+
+class PlaylistAddScreen:Fragment(R.layout.screen_playlist_add_song) {
+}
